648a35ec62804cca4c90935a2c28610f90c4b1a7
Efrata-Habte <efratahabte1213@gmail.com>
2025-09-01 15:46:55 +0300

    feat: implement todo addition functionality with validation and delete option

--- Scripts/Dom.js
+++ Scripts/Dom.js
@@ -6,6 +6,9 @@ const projectForm = document.querySelector('.project-form');
 const addTodoBtn = document.querySelector('#add-todo-btn');
 const cancelTodoBtn = document.querySelector('#cancel-todo-btn'); 
 const todoForm = document.querySelector('.todo-form');
+const todoList= document.querySelector('.todo-list');
+const deleteButtons = document.getElementsByClassName('delete');
+
 
 function showProjectForm() {
   projectForm.classList.remove('hidden'); 
@@ -39,6 +42,39 @@ function addProjectToList() {
     document.querySelector('#project-name').value=''; // Clear input field after adding
 }
 
+function addTodoToList() {
+    const list=document.createElement('li');
+    const todoTitleValue=document.querySelector('#todo-title').value;
+    const todpoDescValue=document.querySelector('#todo-desc').value;
+    const todoDateValue=document.querySelector('#todo-date').value;
+    const todoPriorityValue=document.querySelector('#todo-priority').value;
+
+    if(todoTitleValue.trim()=== '') {
+        alert('Todo title cannot be empty.');
+        return;
+    }
+
+    list.innerHTML=`
+    <div>
+        <span class="title">${todoTitleValue}</span>
+        <br>
+        <p class="description ">${todpoDescValue}</p>
+        <span class="due-date">${todoDateValue ? `Due: ${todoDateValue}` : 'No deadline'}</span>
+        
+    </div>
+    <button class="delete">✖</button>
+    `;
+    list.classList.add('todo', `${todoPriorityValue}-priority`);
+
+    todoList.appendChild(list);
+    document.querySelector('#todo-title').value='';
+    document.querySelector('#todo-desc').value='';
+    document.querySelector('#todo-date').value='';
+    document.querySelector('#todo-priority').value='';
+
+}
+
+// Event Listeners
 
 addProject.addEventListener('click', showProjectForm);
 addProjectBtn.addEventListener('click',(e) => {
@@ -46,7 +82,7 @@ addProjectBtn.addEventListener('click',(e) => {
     addProjectToList();
     hideProjectForm()
     });
-    
+
 cancelProjectBtn.addEventListener('click', (e) => {
     e.preventDefault();
     document.querySelector('#project-name').value=''; // Clear input field on cancel
@@ -55,4 +91,15 @@ cancelProjectBtn.addEventListener('click', (e) => {
 
 addTodo.addEventListener('click', showTodoForm);
 cancelTodoBtn.addEventListener('click', hideTodoForm);
-addTodoBtn.addEventListener('click', hideTodoForm);
+addTodoBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    addTodoToList();
+    hideTodoForm();
+});
+
+document.querySelector('.todo-list').addEventListener('click', (e) =>{
+    if(e.target.classList.contains('delete')) {
+        const todoItem = e.target.closest('li');
+        todoItem.remove();
+    }
+});
